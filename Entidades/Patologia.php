4f8bd16764e9aec1cc6f8a3f611b5a2420d54cee
Fabio Sales <ffabiosales@gmail.com>
2021-06-18 20:15:52 -0300

    mapeia a entidade Patologia

---
+++ Entidades/Patologia.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entidades;
+
+class Patologia
+{
+    private int $id;
+
+    private string $nome;
+
+    private ?string $descricao;
+
+    private int $situacao;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSituacao(): string
+    {
+        return $this->situacao;
+    }
+
+    public function setSituacao(string $situacao): self
+    {
+        $this->situacao = $situacao;
+
+        return $this;
+    }
+
+    // TODO criar método que serialize 
+}
