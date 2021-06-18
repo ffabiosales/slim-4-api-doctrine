38a417111860a5e390e380ea1bf01b868c3a559e
Fabio Sales <ffabiosales@gmail.com>
2021-06-18 19:04:16 -0300

    separa a lógica da api

--- public/index.php
+++ public/index.php
@@ -1,16 +1,5 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = AppFactory::create();
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Simples.vet");
-    return $response;
-});
+require __DIR__ . '/../src/App/App.php';
 
 $app->run();
