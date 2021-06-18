b0313b8f9d82c147fcd0c563af325628138165c7
Fabio Sales <ffabiosales@gmail.com>
2021-06-18 20:07:41 -0300

    cria grupo de rotas e retorno em json slim v4

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -3,8 +3,37 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Simples.vet");
     return $response;
+});
+
+// $app->group('/api/v1', function () use ($app): void {
+
+//     $app->group('/patologias', function () use ($app): void {
+//         $app->get('', function(Request $request, Response $response, $args) {
+
+//             $response->getBody()->write("Simples.vet");
+//             return $response;
+//         });
+//     });
+// });
+
+$app->group('/api/v1', function () use ($app) : void {
+
+    $app->group('/patologias', function ($request, $response, array $args) use ($app) : void {
+
+        $app->get('', function ($request, $response, array $args) {
+            $patologias = [
+                ['nome' => "doença ruim"]
+            ];
+
+            $response->getBody()->write(json_encode($patologias));
+
+            return $response
+                ->withHeader('Content-Type', 'application/json');
+        });
+    });
 });
