25be618b290116c2a8dd95531aae3db046bcf5f5
Fabio Sales <ffabiosales@gmail.com>
2021-06-18 19:11:35 -0300

    separa as rotas em arquivo específico

--- src/App/App.php
+++ src/App/App.php
@@ -1,16 +1,11 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $baseDir = __DIR__ . '/../../';
 
-require $baseDir. 'vendor/autoload.php';
+require $baseDir . 'vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Simples.vet");
-    return $response;
-});
+require __DIR__ . '/Routes.php';
