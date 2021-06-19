410063cdfc8810f6b842e1177a9876137d0bb178
Fabio Sales <ffabiosales@gmail.com>
2021-06-19 16:34:06 -0300

    faz uso de cors/preflight

--- src/App/Rotas.php
+++ src/App/Rotas.php
@@ -6,17 +6,19 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 use App\Controller\PatologiaController;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Simples.vet");
     return $response;
 });
 
-$app->group('/api/v1', function (RouteCollectorProxy $app) {
+$app->group('/api/v1', function (RouteCollectorProxy $group) {
 
     // TODO nested groups tem algum bug no Slim v4?
     // $app->group('/patologias', function () use ($app): void {
-        
+
     //     $app->get('', PatologiaController::class . ':listar');
     //     $app->post('', PatologiaController::class . ':cadastrar');
     //     // $app->get('/{id}', PatologiaController::class . ':mostrar');
@@ -24,11 +26,30 @@ $app->group('/api/v1', function (RouteCollectorProxy $app) {
     //     // $app->delete('/{id}', PatologiaController::class . ':apagar');
     // });
 
-    $app->get('/patologias', PatologiaController::class . ':listar');
-    $app->post('/patologias', PatologiaController::class . ':cadastrar');
-    $app->get('/patologias/{id}', PatologiaController::class . ':mostrar');
-    $app->put('/patologias/{id}', PatologiaController::class . ':atualizar');
-    $app->delete('/patologias/{id}', PatologiaController::class . ':apagar');
+    $group->get('/patologias', PatologiaController::class . ':listar');
+    $group->post('/patologias', 'PatologiaController:cadastrar');
+    // $group->post('/patologias', function (
+    //     ServerRequestInterface $request, 
+    //     ResponseInterface $response
+    //     ): ResponseInterface {
+    //     // Retrieve the JSON data
+    //     $data = (array)$request->getParsedBody();
+
+    //     // Your code here
+    //     $response->getBody()->write('Create user');
+
+    //     return $response;
+    // });
+    // Allow preflight requests for /example
+    $group->options('/patologias', function (
+        ServerRequestInterface $request,
+        ResponseInterface $response
+    ): ResponseInterface {
+        return $response;
+    });
+    $group->get('/patologias/{id}', PatologiaController::class . ':mostrar');
+    $group->put('/patologias/{id}', PatologiaController::class . ':atualizar');
+    $group->delete('/patologias/{id}', PatologiaController::class . ':apagar');
 });
 
 // $app->group('/api/v1', function () use ($app) : void {
