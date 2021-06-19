2a8533cac01c997c386173707a8b3270220330ec
Fabio Sales <ffabiosales@gmail.com>
2021-06-19 00:23:09 -0300

    separa arquivos de configuração

--- src/App/Configuracoes.php
+++ src/App/Configuracoes.php
@@ -1,7 +1,34 @@
 <?php
 
+// https://www.slimframework.com/docs/v3/cookbook/database-doctrine.html
+
+define('APP_ROOT', __DIR__);
+
 return [
     'settings' => [
+        'displayErrorDetails' => true,
+        'determineRouteBeforeAppMiddleware' => false,
 
+        'doctrine' => [
+            // if true, metadata caching is forcefully disabled
+            'dev_mode' => true,
+
+            // path where the compiled metadata info will be cached
+            // make sure the path exists and it is writable
+            'cache_dir' => APP_ROOT . '/var/doctrine',
+
+            // you should add any other path containing annotated entity classes
+            'metadata_dirs' => [APP_ROOT . '/src/Entidades'],
+
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'dbname' => 'simplesvet',
+                'user' => 'root',
+                'password' => '12345678',
+                'charset' => 'utf-8'
+            ]
+        ]
     ]
 ];
